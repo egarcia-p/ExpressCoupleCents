861e46068b5d94f69348e98bf17c2397a7b84c8f
egarcia-p <eugenio5039@gmail.com>
2024-01-01 21:36:15 -0600

    Add - Transaction model svc

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.familyController = require('./family.controller');
+module.exports.transactionController = require('./transaction.controller');
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const familyRoute = require('./family.route');
+const transactionRoute = require('./transaction.route');
 const docsRoute = require('./docs.route');
 const config = require('../../config/config');
 
@@ -19,6 +20,10 @@ const defaultRoutes = [
   {
     path: '/families',
     route: familyRoute,
+  },
+  {
+    path: './transactions',
+    route: transactionRoute,
   },
 ];
 
